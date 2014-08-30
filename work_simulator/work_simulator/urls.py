4741e32d5a913c25f18c4b1456ebd9d66c78339f
Glider <mail@marcelcarl.de>
2014-08-30 19:21:15 +0200

    Add a hello world view

--- work_simulator/work_simulator/urls.py
+++ work_simulator/work_simulator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'work_simulator.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('game.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
