9dc1f002fb297045e10e5239a9acf00a2ad4f862
Alex Dik <mail@alexdik.de>
2014-09-06 21:26:10 +0200

    Refactoring

--- work_simulator/game/views.py
+++ work_simulator/game/views.py
@@ -4,15 +4,7 @@ from django.template import RequestContext, loader
 
 def page(request, parent='', sub=''):
     """View for pages"""
-    parent = parent or 'pages'
-    sub = sub or 'index'
+    parent, sub = parent or 'pages', sub or 'index'
     template = loader.get_template('{}/{}.html'.format(parent, sub))
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-
-def game(request, sub=''):
-    sub = sub or 'index'
-    template = loader.get_template('game/{}.html'.format(sub))
     context = RequestContext(request)
     return HttpResponse(template.render(context))
