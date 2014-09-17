309d40b9bb7a8e12ab5de1bcb7c436707d60c680
Glider <mail@marcelcarl.de>
2014-09-17 21:10:44 +0200

    Implement a super simple login mechanism

--- src/app.py
+++ src/app.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, escape
 
 app = Flask(__name__)
-
+app.secret_key = 'Bruce Schneier knows Alice and Bob\'s shared secret.'
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+
+    return redirect('login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        if not username:
+            error = 'Username can\'t be blank!'
+        else:
+            session['username'] = username
+            return redirect('')
+
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
