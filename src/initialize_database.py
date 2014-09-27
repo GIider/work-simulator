a0c8525f8487ae0472f72d54e7f1bd1ac65b6e30
Glider <mail@marcelcarl.de>
2014-09-27 18:13:33 +0200

    Fix database initializing script not initalizing everyone

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -12,4 +12,4 @@ for player_name in PLAYER_NAMES:
 trainee = models.Entity.create_if_not_exists(name='Azubi', money_generated=1, money_cost=0, money_interval=5)
 
 for player_id in players:
-    models.OwnedEntities.set_owned_amount_for_player(player_id=player.id, entity_id=trainee.id, amount=5)
+    models.OwnedEntities.set_owned_amount_for_player(player_id=player_id, entity_id=trainee.id, amount=5)
