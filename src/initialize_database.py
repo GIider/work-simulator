540167904f13b24be3c8f82719f0eff74bdde6a1
Glider <mail@marcelcarl.de>
2014-09-27 02:28:38 +0200

    Don't hardcode the ids

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from work_simulator import models
 
-models.Player.create_if_not_exists(username='Glider')
-models.Entity.create_if_not_exists(name='Azubi', money_generated=1, money_cost=0, money_interval=5)
+glider = models.Player.create_if_not_exists(username='Glider')
+trainee = models.Entity.create_if_not_exists(name='Azubi', money_generated=1, money_cost=0, money_interval=5)
 
-models.OwnedEntities.set_owned_amount_for_player(player_id=1, entity_id=1, amount=5)
+models.OwnedEntities.set_owned_amount_for_player(player_id=glider.id, entity_id=trainee.id, amount=5)
